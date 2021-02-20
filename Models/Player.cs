085c034932664aa61a9585d4bed09c5552295885
thesayedfahad <thesayedfahad@gmail.com>
2021-02-19 18:15:53 -0800

    Fixed bug  for second page

--- Models/Player.cs
+++ Models/Player.cs
@@ -4,7 +4,7 @@ namespace assignment01.Models
     {
         public Person person {get; set;}
 
-        public string jersey {get; set;}
+        public string jerseyNumber {get; set;}
 
         public Position position {get; set;}
     }
