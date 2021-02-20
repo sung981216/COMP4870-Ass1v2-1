4d97975878c5c6468dd14b294ef6a5c4d18053e8
thesayedfahad <thesayedfahad@gmail.com>
2021-02-19 20:19:27 -0800

    Added third page and fixed bug

--- Models/People.cs
+++ Models/People.cs
@@ -5,7 +5,7 @@ namespace assignment01.Models
         public string fullName {get; set;}
         public Team currentTeam {get; set;}
         public int currentAge {get; set;}
-        public int nationality {get; set;}
+        public string nationality {get; set;}
         public Position primaryPosition {get; set;}
     }
 }
